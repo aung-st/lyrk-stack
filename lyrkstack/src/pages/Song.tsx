986d8c9d4db91be036d7270bfb7cf2ff668e5818
aung-st <s.taung1445@gmail.com>
2026-01-08 22:02:04 +0000

    feat: draft button toggle and tweak package json

--- lyrkstack/src/pages/Song.tsx
+++ lyrkstack/src/pages/Song.tsx
@@ -1,6 +1,7 @@
 import { useLocation } from "react-router-dom"
 import "../styles/Song.css"
 
+
 function Song() {
     const location = useLocation()
     const { from } = location.state
@@ -8,7 +9,6 @@ function Song() {
     const songDetails = from.item
     const buttonList = []
 
-    console.log(songDetails.translation[0])
 
     // Create a button for every language available including transliterations
     for (let index = 0; index < songDetails.translation.length; index++) {
@@ -19,14 +19,20 @@ function Song() {
         )
     }
 
+    // change translation text in h4 tag depending on what button you press and the index of that button in the button list relative to translations in song details
+    function toggleTranslation(){
+        let button = document.querySelectorAll(".button-list")
+        console.log(button)
+    }
+    
     return (
-        <>
+        <>  
             <div className="song-header">
                 <h1>
                     id: {songDetails.id}: title: {songDetails.title}
                 </h1>
             </div>
-            <div className="song-wrapper">
+            <div className="song-wrapper" onClick = {toggleTranslation}>
                 <div className="lyric left">
                     <ul className="button-list">{buttonList}</ul>
                     <h4>{songDetails.translation[0].lyrics}</h4>
