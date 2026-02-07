da05352c2b8d11525e86b91df35d0df199857ebb
aung-st <s.taung1445@gmail.com>
2026-02-07 00:46:56 +0000

    refactor: tweak about page layout

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -12,12 +12,13 @@ function About() {
                 </p>
                 <p>
                     With Lyrk Stack, adding your own translations is a walk in the
-                    park! Simply , where you can easily create new translations by
-                    entering your text and selecting the target language. Once you've
-                    added your translations, viewing them is just as straightforward.
-                    Everything is neatly organized, so you can quickly access and
-                    manage your entries. Whether you're updating existing
-                    translations or browsing through your collection.
+                    park! Simply fill out a form on the "Add Song" page where you can
+                    easily create new translations by entering your text and
+                    selecting the target language. Once you've added your
+                    translations, viewing them is just as straightforward. Everything
+                    is neatly organized, so you can quickly access and manage your
+                    entries. Whether you're updating existing translations or
+                    browsing through your collection.
                 </p>
                 <p>
                     Lyrk Stack makes it super convenient to keep track of all your
@@ -28,7 +29,7 @@ function About() {
                         You can:
                         <ul>
                             <li>Add as many translations as you want to a song.</li>
-                            <li>Refine translations existing lyrics.</li>
+                            <li>Refine translations for existing lyrics.</li>
                             <li>Export all your song data for a rainy day!</li>
                         </ul>
                     </div>
